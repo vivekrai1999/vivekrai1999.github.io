eb747fafe570ccaf77ff60d6d745ce89ff881fea
Vivek Rai <96563624+vivekrai1999@users.noreply.github.com>
2024-03-17 21:07:54 +0530

    added responsive design

--- sundown-studio/main.js
+++ sundown-studio/main.js
@@ -130,13 +130,13 @@ document.querySelector(".swiper").addEventListener("mouseleave", () => {
 
 const services = {
   design: {
-    text: "Our team works with our clients to refine an idea and concept into an executable design. We create a final design that encompasses the brand narrative to bring stories to life and provide end-to-end design solutions from concept, design, and architectural drawings to 3D renderings.",
+    text: "Our team works with our clients to refine an idea and concept into an executable design. We create a final design that encompasses the brand narrative to bring stories to life and provide end-to-end design solutions from concept.",
     image:
       "https://assets-global.website-files.com/64d3dd9edfb41666c35b15b7/64d3dd9edfb41666c35b15e1_Project.webp",
   },
 
   project: {
-    text: "Once we have a design, our production team takes the lead in bringing it to life. We manage all stages of the project, from build specifications and technical drawings to site surveys, vendor management, and 2D & 3D production. We have an extensive network of partners to meet each unique design and project need.",
+    text: "Once we have a design, our production team takes the lead in bringing it to life. We manage all stages of the project, from build specifications and technical drawings to site surveys, vendor management, and 2D & 3D production.",
     image:
       "https://assets-global.website-files.com/64d3dd9edfb41666c35b15b7/64d3dd9edfb41666c35b15d0_Project.webp",
   },
@@ -150,6 +150,7 @@ const services = {
 
 document.querySelectorAll(".services-option .option").forEach((element) =>
   element.addEventListener("click", (e) => {
+    e.stopPropagation();
     document
       .querySelectorAll(".services-option .option")
       .forEach((elem) => elem.classList.remove("active-service"));
